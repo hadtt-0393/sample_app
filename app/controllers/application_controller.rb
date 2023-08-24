de9af125070c62adf4e600f0a8f2aacdda23d53b
Thu Ha <dinh.thi.thu.ha@sun-asterisk.com>
2023-08-24 17:17:13 +0700

    Chapter 10

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
+  include Pagy::Backend
   before_action :set_locale
 
   private
@@ -11,5 +12,14 @@ class ApplicationController < ActionController::Base
 
   def default_url_options
     {locale: I18n.locale}
+  end
+
+  # Confirms a logged-in user.
+  def logged_in_user
+    return if logged_in?
+
+    store_location
+    flash[:danger] = t ".please_login"
+    redirect_to login_url, status: :see_other
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,22 +1,31 @@
 class SessionsController < ApplicationController
+  before_action :find_user, only: %i(create)
   def new; end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user&.authenticate params[:session][:password]
+    if @user.authenticate params.dig(:session, :password)
+      forwarding_url = session[:forwarding_url]
       reset_session
-      log_in user
-      redirect_to user
+      params[:session][:remember_me] == "1" ? remember(@user) : forget(@user)
+      log_in @user
+      redirect_to forwarding_url || @user
     else
-      flash.now[:danger] = t ".invalid_login" # Not quite right!
-      render "new", status: :unprocessable_entity
+      flash.now[:danger] = t ".invalid_login"
+      render :new, status: :unprocessable_entity
     end
   end
 
   def destroy
-    return unless logged_in?
-
-    log_out
+    log_out if logged_in?
     redirect_to root_url, status: :see_other
+  end
+
+  private
+  def find_user
+    @user = User.find_by email: params.dig(:session, :email)&.downcase
+    return unless @user.nil?
+
+    flash[:danger] = t ".invalid_login"
+    redirect_to action: :new
   end
 end
